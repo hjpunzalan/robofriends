907cad40489be14a4ebd29dc9ce8548b91805b31
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-03-09 17:43:44 +0800

    Updates

---
+++ precache-manifest.d7b291132eb0257ed7fea0a075da875c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.dec20aab.js"
+  },
+  {
+    "revision": "74b930068b7dd43f0285",
+    "url": "/robofriends/static/js/main.d3040a0d.chunk.js"
+  },
+  {
+    "revision": "bdc0191894262a2d2770",
+    "url": "/robofriends/static/js/2.0eeea03b.chunk.js"
+  },
+  {
+    "revision": "74b930068b7dd43f0285",
+    "url": "/robofriends/static/css/main.c47600fa.chunk.css"
+  },
+  {
+    "revision": "bdc0191894262a2d2770",
+    "url": "/robofriends/static/css/2.047519e1.chunk.css"
+  },
+  {
+    "revision": "387df013159ad80a6c9ee9925f24ad30",
+    "url": "/robofriends/index.html"
+  }
+];
